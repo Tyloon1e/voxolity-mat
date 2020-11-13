b6559ff48f95f049491614be9005b56e8159b2c8
Tyloon1e <52511724+Tyloon1e@users.noreply.github.com>
2020-11-13 22:56:05 +0100

    Decreasing delay

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,9 +1,9 @@
 // SHOWING ELEMENTS WHEN PAGE IS DONE LOADING
 
 $(window).on("load", function () {
-  $(".progress").delay(1500).fadeOut("quick");
+  $(".progress").delay(500).fadeOut("quick");
   $(".hidden")
-    .delay(2000)
+    .delay(1000)
     .queue(function (next) {
       $(this).addClass("visible");
       $(this).addClass("fade-in");
