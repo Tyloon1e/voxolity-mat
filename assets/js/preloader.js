8bd287355d89e4de79a0c4b3bbd6f9a2c25ed8b6
Tyloon1e <52511724+Tyloon1e@users.noreply.github.com>
2020-11-26 12:08:02 +0100

    Remove delay

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,13 +1,11 @@
 // SHOWING ELEMENTS WHEN PAGE IS DONE LOADING
 
 $(window).on("load", function () {
-  $(".progress").delay(500).fadeOut("quick");
-  $(".hidden")
-    .delay(1000)
-    .queue(function (next) {
-      $(this).addClass("visible");
-      $(this).addClass("fade-in");
-      $(this).removeClass("hidden");
-      next();
-    });
+  $(".progress").fadeOut("quick");
+  $(".hidden").queue(function (next) {
+    $(this).addClass("visible");
+    $(this).addClass("fade-in");
+    $(this).removeClass("hidden");
+    next();
+  });
 });
