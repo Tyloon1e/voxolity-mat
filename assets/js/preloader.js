4685dd12a73cbabd96049f5c3b4e4af17fb2b95f
Tyloon1e <christian.stangerup@icloud.com>
2021-03-07 12:46:13 +0100

    Various updates to improve site

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,9 +1,9 @@
 // SHOWING ELEMENTS WHEN PAGE IS DONE LOADING
 
 $(window).on("load", function () {
-  $(".progress").delay(500).fadeOut("quick");
+  $(".progress").delay(200).fadeOut("quick");
   $(".hidden")
-    .delay(1000)
+    .delay(600)
     .queue(function (next) {
       $(this).addClass("visible");
       $(this).addClass("fade-in");
